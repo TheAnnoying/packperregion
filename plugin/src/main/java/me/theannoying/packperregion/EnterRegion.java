eb7556499d6f098dc20211bccab6193ad38a4911
TheAnnoying <drork2018@gmail.com>
2026-01-06 01:35:47 +0200

    feat: added applying pack optionally

--- plugin/src/main/java/me/theannoying/packperregion/EnterRegion.java
+++ plugin/src/main/java/me/theannoying/packperregion/EnterRegion.java
@@ -1,6 +1,11 @@
 package me.theannoying.packperregion;
 
 import com.google.gson.JsonArray;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,18 +21,26 @@ import static me.theannoying.packperregion.Util.*;
 
 public class EnterRegion implements Listener {
 	private static final HashMap<UUID, String> regionEntered = new HashMap<>();
-	private void applyPack(Player player, Location playerLocation) {
+	public static void applyPack(Player player, Location playerLocation, boolean force) {
 		getPackList().forEach(pack -> {
 			if (pack.getAsJsonObject().get("pack_status").getAsString().equals("Accepted")) {
 				JsonArray coordArray = pack.getAsJsonObject().get("coordinates").getAsJsonArray();
 				boolean isWithinRegion = isWithinRegion(coordArray.get(0).getAsJsonArray(), coordArray.get(1).getAsJsonArray(), playerLocation);
 				String id = pack.getAsJsonObject().get("id").getAsString();
 
-				if (!id.equals(regionEntered.get(player.getUniqueId())) && isWithinRegion) {
-					regionEntered.put(player.getUniqueId(), id);
-					player.addResourcePack(UUID.fromString(pack.getAsJsonObject().get("id").getAsString()), pack.getAsJsonObject().get("pack_url").getAsString(), null, null, getConfigBool("settings.make_region_packs_optional"));
+				if ((!id.equals(regionEntered.get(player.getUniqueId())) || force) && isWithinRegion) {
+                    regionEntered.put(player.getUniqueId(), id);
+                    if(getConfigBool("settings.make_region_packs_required") || force) {
+                        player.addResourcePack(UUID.fromString(pack.getAsJsonObject().get("id").getAsString()), pack.getAsJsonObject().get("pack_url").getAsString(), null, null, getConfigBool("settings.make_region_packs_required"));
+                    } else {
+                        TextComponent component = new TextComponent(ChatColor.translateAlternateColorCodes('&', getConfigString("messages.request_allow_pack_clickable")));
+                        component.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/packperregion applypack"));
+
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfigString("messages.request_allow_pack")));
+                        player.spigot().sendMessage(component);
+                    }
 				} else if(id.equals(regionEntered.get(player.getUniqueId())) && !isWithinRegion) {
-					regionEntered.remove(player.getUniqueId());
+                    regionEntered.remove(player.getUniqueId());
 					player.removeResourcePack(UUID.fromString(pack.getAsJsonObject().get("id").getAsString()));
 				}
 			}
@@ -42,7 +55,7 @@ public class EnterRegion implements Listener {
 		Player player = event.getPlayer();
 		Location to = event.getTo();
 
-		applyPack(player, to);
+        applyPack(player, to, false);
 	}
 
 	@EventHandler
@@ -50,7 +63,7 @@ public class EnterRegion implements Listener {
 		Player player = event.getPlayer();
 		Location location = player.getLocation();
 
-		applyPack(player, location);
+        applyPack(player, location, false);
 	}
 
 	@EventHandler
@@ -58,6 +71,6 @@ public class EnterRegion implements Listener {
 		Player player = event.getPlayer();
 		Location location = player.getLocation();
 
-		applyPack(player, location);
+        applyPack(player, location, false);
 	}
 }
